c9184de020e1d5a82e7f8e7f44a3fc6b01e23573
Tim Sutton <tim@linfiniti.com>
2013-08-28 22:58:17 +0200

    #53 The new web site must be deployed on linfiniti3 - refactored fabfile and apache template

--- fabfile.py
+++ fabfile.py
@@ -1,118 +1,55 @@
 # ~/fabfile.py
-# A Fabric file for carrying out various administrative tasks with InaSAFE.
+"""A Fabric file for carrying out various administrative tasks with InaSAFE.
+
+e.g.::
+
+    fab -H foo:1234 setup_docs_web_site
+
+.. note:: Vagrant tasks will only run if they @task decorator is used on
+   the function. See show_environment function below.
+
+"""
 # Tim Sutton, Jan 2013
 
 import os
 from fabric.api import *
-from fabric.contrib.files import contains, exists, append, sed
+from fabric.contrib.files import contains, exists, append, upload_template
+from fabric.colors import green
 import fabtools
 from fabtools import require
 # noinspection PyUnresolvedReferences
 from fabgis.qgis import install_qgis1_8
 from fabgis.jenkins import jenkins_deploy_website, install_jenkins
-from fabgis.utilities import replace_tokens
 from fabgis.git import update_git_checkout
 from fabgis.inasafe import setup_inasafe
 from fabgis.sphinx import setup_latex
-from fabgis.docker import (
-    setup_docker, start_docker_daemon, create_docker_container)
+# noinspection PyUnresolvedReferences
+from fabgis.docker import setup_docker, create_docker_container
 # Don't remove even though its unused
 # noinspection PyUnresolvedReferences
 from fabtools.vagrant import vagrant
 
-# Usage for localhost commands:
-# fab localhost [command]
-#  e.g. fab localhost update
-# To run remotely do
-#  fab remote [command]
-# e.g.
-# fab -H 188.40.123.80:8697 show_environment
-# To run on a vagrant vhost do
-# fab vagrant show_environment
-#  e.g. fab vagrant show_environment
-#
-# Note: Vagrant tasks will only run if they @task decorator is used on
-#       the function. See show_environment function below.
-
-# Usage fab localhost [command]
-#    or fab remote [command]
-#  e.g. fab localhost initialise_qgis_plugin_repo
-
 # Global options
-env.env_set = False
-
-@task
-def localhost():
-    """Simple convenient alias for running commands on localhost."""
-    env.hosts = ['%s@localhost' % env.user]
-
-
-def _setup_env():
-    """Things to do regardless of whether command is local or remote."""
-    if env.env_set:
-        fastprint('Environment already set!\n')
-        return
-
-    fastprint('Setting environment!\n')
-    # Key is hostname as it resolves by running hostname directly on the server
-    # value is desired web site url to publish the repo as.
-    doc_site_names = {
-        'cunonia': 'docs.localhost',
-        'waterfall': 'docs.localhost',
-        'spur': '-docs.localhost',
-        'maps.linfiniti.com': 'inasafe.linfiniti.com',
-        'linfiniti': 'docs.linfiniti.com',
-        #vagrant instance
-        'vagrant-inasafe-doc': 'docs.vagrant.localhost',
-        'shiva': 'docs.inasafe.org'}
-
-    with hide('output'):
-        env.user = run('whoami')
-        env.hostname = run('hostname')
-        if env.hostname not in doc_site_names:
-            print 'Error: %s not in: \n%s' % (env.hostname, doc_site_names)
-            exit()
-        else:
-            env.doc_site_name = doc_site_names[env.hostname]
-            env.inasafe_release_web_path = (
-                '\/var\/www\/inasafe-release-documentation')
-            env.inasafe_master_web_path = (
-                '\/var\/www\/inasafe-master-documentation')
-            env.home = os.path.join('/home/', env.user)
-            env.repo_path = os.path.join(env.home, 'dev', 'python')
-            env.git_url = 'git://github.com/AIFDR/inasafe-doc.git'
-            env.repo_alias = 'inasafe-doc'
-            env.code_path = os.path.join(env.repo_path, env.repo_alias)
-
-    env.env_set = True
-    fastprint('env.env_set = %s' % env.env_set)
-
-###############################################################################
-# Next section contains helper methods
-###############################################################################
-
-
-def get_webdir(branch):
-    if 'master' == branch:
-        webdir = env.inasafe_master_web_path
-    else:
-        webdir = env.inasafe_release_web_path
-    return webdir
+repo_path = os.path.join(env.home, 'dev', 'python')
+code_path = os.path.join(env.repo_path, 'inasafe-doc')
+web_directory = os.path.join('var', 'www', 'inasafe-org')
 
 
 @task
 def build_docs(branch='master'):
     """Create a pdf and html doc tree and publish them online.
-    Args:
-        branch: str - a string representing the name of the branch to build
-            from. Defaults to 'master'.
+
+    :param branch: The name of the branch to build from. Defaults to 'master'.
+    :type branch: str
+
     To run e.g.::
+
         fab -H 188.40.123.80:8697 build_docs
         or to package up a specific branch (in this case minimum_needs)
         fab -H 88.198.36.154:8697 build_docs:version-1_1
+
     .. note:: Using the branch option will not work for branches older than 1.1
     """
-    _setup_env()
     setup_inasafe()
     # Needed for when running on headless servers
     fabtools.require.deb.package('xvfb')
@@ -121,9 +58,9 @@ def build_docs(branch='master'):
     sudo('pip install Sphinx')
 
     update_git_checkout(
-        code_path=env.repo_path,
-        url=env.git_url,
-        repo_alias=env.repo_alias,
+        code_path=repo_path,
+        url='git://github.com/AIFDR/inasafe-doc.git',
+        repo_alias='inasafe-doc',
         branch=branch)
     setup_latex()
 
@@ -135,17 +72,20 @@ def build_docs(branch='master'):
 
 
 @task
-def setup_docs_webpage(branch='master'):
-    """Initialise an InaSAFE docs site where we host docs and pdf."""
-    _setup_env()
+def setup_docs_web_site(branch='master'):
+    """Initialise an InaSAFE docs site where we host docs and pdf.
+
+    :param branch: Which branch of the documentation to build.
+    :type branch: str
+
+    """
     build_docs()
 
     fabtools.require.deb.package('apache2')
     code_path = env.code_path
-    webdir = get_webdir(branch)
 
-    # if hostname = jenkins or vagrant or livesite then
-    # webdir = /var/www/inasafe-documentation
+    # if hostname = jenkins or vagrant or live site then
+    # web_directory = /var/www/inasafe-documentation
     # for now we set dir just for jenkins anyway
     # probably we should just set a symlink
 
@@ -155,41 +95,44 @@ def setup_docs_webpage(branch='master'):
         sudo('chown -R %s.%s %s' % (env.user, env.user, jenkins_webdir))
         sudo('chmod -R 777 %s' % jenkins_webdir)
 
-    inasafe_docs_apache_conf = '%s.inasafe-docs.conf' % branch
-    inasafe_docs_apache_conf_template = 'inasafe-docs.conf.templ'
+    apache_conf_template = 'inasafe-docs.conf.templ'
 
-    if not exists(webdir):
-        sudo('mkdir -p %s/pdf' % webdir)
-        sudo('chown -R %s.%s %s' % (env.user, env.user, webdir))
+    if not exists(web_directory):
+        sudo('mkdir -p %s/pdf' % web_directory)
+        sudo('chown -R %s.%s %s' % (env.user, env.user, web_directory))
 
     apache_path = '/etc/apache2/sites-available/'
-    with cd(apache_path):
-        if not exists(inasafe_docs_apache_conf):
-            local_dir = os.path.dirname(__file__)
-            local_file = os.path.abspath(os.path.join(
-                local_dir,
-                'scripts',
-                inasafe_docs_apache_conf_template))
-            put(local_file,
-                "/etc/apache2/sites-available/%s" %
-                inasafe_docs_apache_conf_template,
-                use_sudo=True)
 
-        my_tokens = {
-            'SERVERNAME': env.doc_site_name,  # Web Url e.g. foo.com
-            'WEBMASTER': 'werner@linfiniti.com',  # email of web master
-            'DOCUMENTROOT': webdir,  # Content root .e.g. /var/www
-            'SITENAME': 'inasafe-docs',  # Choosen name of jenkins 'root'
-        }
-        replace_tokens(inasafe_docs_apache_conf_template, my_tokens)
-        sudo('rm *.templ')
+    # Clone and replace tokens in apache conf
+
+    local_dir = os.path.dirname(__file__)
+    local_file = os.path.abspath(os.path.join(
+        local_dir,
+        'scripts',
+        apache_conf_template))
+
+    context = {
+        'server_name': env.doc_site_name,  # Web Url e.g. foo.com
+        'web_master': 'info@inasafe.org',  # email of web master
+        'document_root': web_directory,  # Content root .e.g. /var/www
+    }
+
+    fastprint(green('Using %s for template' % local_file))
+
+    destination = '%s/inasafe-docs.apache.conf' % apache_path
+
+    upload_template(
+        local_file,
+        destination,
+        context=context,
+        use_sudo=True)
 
     with cd(code_path):
         # Copy built Documentation to the Webserver path
-        run('cp -r docs/output/html/* %s' % webdir)
-        run('cp -r docs/output/pdf %s' % webdir)
-        run('cp scripts/.htaccess %s' % webdir)
-        run('cp scripts/directory*.html %s/en/_static/' % webdir)
+        run('cp -r docs/output/html/* %s' % web_directory)
+        run('cp -r docs/output/pdf %s' % web_directory)
+        run('cp scripts/.htaccess %s' % web_directory)
+        run('cp scripts/directory*.html %s/en/_static/' % web_directory)
 
     # Add a hosts entry for local testing - only really useful for localhost
     hosts = '/etc/hosts'
@@ -203,14 +146,15 @@ def setup_docs_webpage(branch='master'):
 
 @task
 def setup_jenkins(use_upstream_repo=True, branch='master'):
-    """
+    """Setup a jenkins build job to build the docs.
 
-    :param use_upstream_repo:
-    :param branch:
-    :return:
-    """
-    _setup_env()
+    :param use_upstream_repo: Whether to use apt package for jenkins or the
+        upstream original installer.
+    :type use_upstream_repo: bool
 
+    :param branch: Which branch of the documentation to build.
+    :type branch: str
+    """
     # We need some additional tools to run jenkins checks for documentation
     fabtools.require.deb.package('xvfb')
     fabtools.require.deb.package('pep8')
@@ -225,12 +169,11 @@ def setup_jenkins(use_upstream_repo=True, branch='master'):
 
 @task
 def setup_jenkins_jobs(branch='master'):
+    """Uploads the prepared jobs into jenkins for testing.
+
+    :param branch: Which branch of the documentation to build.
+    :type branch: str
     """
-    uploads the prepared jobs into jenkins for testing
-    :param branch: which branch of the documentations
-    :return:
-    """
-    _setup_env()
     xvfb_config = "org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml"
     job_dir = ['InaSAFE-Documentation']
     with cd('/var/lib/jenkins/'):
@@ -252,12 +195,11 @@ def setup_jenkins_jobs(branch='master'):
                     local_dir,
                     'scripts',
                     'jenkins_jobs',
-                    '%s.xml') % job)
+                    '%s.xml' % job))
                 sudo('mkdir /var/lib/jenkins/jobs/%s' % job)
                 put(local_job_file,
                     "/var/lib/jenkins/jobs/%s/config.xml" % job,
                     use_sudo=True)
         sudo('chown -R jenkins:nogroup InaSAFE*')
     sudo('service jenkins restart')
-    webdir = get_webdir(branch)
-    fabtools.require.directory(webdir, use_sudo=True, owner='jenkins')
+    fabtools.require.directory(web_directory, use_sudo=True, owner='jenkins')
